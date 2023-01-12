4c84f367aff86b4606e5c4399826a61cdd164d50
EquinoxM <equinoxm.02@gmail.com>
2023-01-12 15:44:58 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/equinoxm/world_time/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.equinoxm.world_time
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/loading.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(
+        location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'time': instance.time,
+      'location': instance.location,
+      'flag': instance.flag,
+      'isDayTime': instance.isDayTime,
+    });
+    // print(instance.time);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitSpinningLines(
+          color: Colors.white,
+          size: 70.0,
+          lineWidth: 3.0,
+        ),
+      ),
+    );
+  }
+}
